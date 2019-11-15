78acfddde8ed3d297fb139c395c2bdbfe8c6dc8d
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2019-11-15 14:02:59 -0500

    i will need extra time to finish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
-
+import CharacterList from "./components/CharacterList.js";
+import WelcomePage from "./components/WelcomePage.js";
+import SearchForm from "./components/SearchForm.js";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route path="/" component={WelcomePage} />
+      <Route path="/char" component={CharacterList} />
+      <Route path="/search" component={SearchForm} />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,53 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Route } from "react-router-dom";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
 
 export default function CharacterList() {
+  const [chars, setChars] = useState([]);
   // TODO: Add useState to track data from useEffect
 
   useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        setChars(response.data.results);
+        console.log(response.data.results);
+      })
+      .catch(error => {
+        console.log("the data and result was not returned", error);
+      });
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {chars.map(char => (
+        <charDetails key={char.id} char={char} />
+      ))}
     </section>
+  );
+}
+
+function charDetails({ char }) {
+  return (
+    <Card className="char-card">
+      <CardBody>
+        <CardTitle>{char.name}</CardTitle>
+        <CardSubtitle>Status: {char.status}</CardSubtitle>
+        <CardSubtitle>Spacies: {char.species}</CardSubtitle>
+        <CardSubtitle>Origin: {char.origin.name}</CardSubtitle>
+        <CardSubtitle>Current Location: {char.location.name}</CardSubtitle>
+      </CardBody>
+    </Card>
   );
 }
